b65a5644e97f34a4b8529ba3d7380801076c982e
Arlony Diaz <arlonydiaz@gmail.com>
2021-06-29 01:59:47 -0500

    filter v1

--- src/main.js
+++ src/main.js
@@ -3,6 +3,29 @@ import { createDropdownTypes, createPokemonList } from './dom/index.js';
 
 const addListeners = () => {
 	// Code goes here
+	//Type Filter
+	const dropdownTypes = document.getElementById("dropdown-types");
+	dropdownTypes.addEventListener('change', typeShown);
+
+	function typeShown(event) {
+		var pokeType = event.target.value;
+		var cardItems = document.getElementsByClassName('card-list__item');
+
+		for(let i = 0; i < cardItems.length; i++) {
+
+			if(pokeType === 'all'){
+				cardItems[i].classList.remove('hide');
+				//console.log(`mostrando tipo: ${pokeType}`);
+			} else if (cardItems[i].getAttribute('data-types').includes(pokeType)) {
+				cardItems[i].classList.remove('hide');
+				//console.log(`mostrando tipo: ${pokeType}`);
+			} else {
+				cardItems[i].classList.add('hide');
+			}
+
+		}
+	}
+	//Type Filter ends
 };
 
 const main = async () => {
