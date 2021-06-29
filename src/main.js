f8ae8cb64fc35f235e19d981bd87125266989e2d
Arlony Diaz <arlonydiaz@gmail.com>
2021-06-29 02:34:03 -0500

    theme solution v1

--- src/main.js
+++ src/main.js
@@ -3,6 +3,22 @@ import { createDropdownTypes, createPokemonList } from './dom/index.js';
 
 const addListeners = () => {
 	// Code goes here
+	
+	//Theme change
+	const themeSwitch = document.getElementById('header-checkbox');
+	themeSwitch.addEventListener('change', changeTheme);
+
+	function changeTheme(event) {
+		//var status = event.target.checked;
+		var mainContent = document.querySelector(".main-content");
+		var navBar = document.querySelector(".header__nav");
+
+		mainContent.classList.toggle('dark');
+		navBar.classList.toggle('dark');
+		//console.log(status);
+	}
+	//Theme change ends	
+
 };
 
 const main = async () => {
