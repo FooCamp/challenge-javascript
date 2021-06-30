cf34334ae3dad0ba394afc35bdef37f6ca7baff2
German Ramirez <gramirez@H12125.ad.hugeinc.com>
2021-06-30 15:50:34 -0500

    Adding the Theme functionality to the code

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import { getPokemonList } from './api/index.js';
 import { createDropdownTypes, createPokemonList } from './dom/index.js';
 
-const addListeners = () => {
-	// Code goes here
+// Function to toggle theme based on the checkbox selection
+const toggleTheme = () => {
+	// Select checkbox from DOM
+	const checkbox = document.querySelector('.checkbox__input');
+	// Select element that has the class main-content which is the one that needs to be toggled with the theme
+	const mainContent = document.querySelector('.main-content');
+	// Define the class that needs to be added/removed from the main-content
+	const darkClass = "dark";
+
+	//Event listener that toggles the theme based on the Click event
+	checkbox.addEventListener('click', () => {
+		mainContent.classList.toggle('dark');
+	});
 };
 
 const main = async () => {
@@ -13,7 +24,7 @@ const main = async () => {
 	createPokemonList(pokemonList, app);
 	createDropdownTypes(pokemonList, dropdownTypes);
 
-	addListeners();
+	toggleTheme();
 };
 
 main();
