c987b24c8e8fea9354b65920517bd9b15ba5643b
German Ramirez <gramirez@H12125.ad.hugeinc.com>
2021-07-01 18:52:43 -0500

    Resolving comments and refactoring code to make it simpler based on the recommendations

--- src/main.js
+++ src/main.js
@@ -3,41 +3,35 @@ import { createDropdownTypes, createPokemonList } from './dom/index.js';
 
 const filterCards = () => {
 	// Define variable to select the Dropdown from the DOM.
-	const dropdownType = document.getElementById("dropdown-types"); 
+	const dropdownType = document.getElementById('dropdown-types'); 
 
 	// Add the event listener to the Dropdown to catch when the selection changes and filter the list based on the selection
 	dropdownType.addEventListener('change', (event) => {
 		// Constant with the Card list item DOM element
-		const cardsList = document.querySelectorAll(".card-list__item");
+		const cardsList = document.querySelectorAll('.card-list__item');
 		// Variable to store the value selected in the dropdown
 		let currentSelection = dropdownType.value;
 		// Variable to store the type of the pokemon that is being evaluated
-		let currentPokemonType = "";
+		let currentPokemonType = '';
 		
 		// Logic to determine wheter the element should have the hide class or not
 		// If the selection is "all", the hide class should be removed from all the elements
 		// If the selection is a type, the "hide"class should be added to the pokemons that do not have that type
 		// and should be removed from the pokemons that have that type.
-		if (currentSelection == "all") {
-			cardsList.forEach(element => {
-					element.classList.remove("hide");
-			});
-		} else {
-			cardsList.forEach(element => {
-				currentPokemonType = element.attributes[0].value;
-				if (currentPokemonType.includes(currentSelection)) {
-						element.classList.remove("hide");
-				} else {
-						element.classList.add("hide");
-				}
-			});
-		}
+		cardsList.forEach(element => {
+			currentPokemonType = element.getAttribute('data-types');
+			if (currentSelection === 'all') {
+				element.classList.remove('hide');
+			} else {
+				element.classList.toggle('hide', !currentPokemonType.includes(currentSelection));
+			}
+		});
 	});
 };
 
 const main = async () => {
-	const app = document.getElementById("app");
-	const dropdownTypes = document.getElementById("dropdown-types");
+	const app = document.getElementById('app');
+	const dropdownTypes = document.getElementById('dropdown-types');
 	const pokemonList = await getPokemonList();
 	
 	createPokemonList(pokemonList, app);
