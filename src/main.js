713ef24ee08f587a2e587b0aa62911566476ea93
MARIAN QUEVEDO <mariansofia1999@hotmail.com>
2021-07-01 17:57:58 -0500

    add theme functionality

--- src/main.js
+++ src/main.js
@@ -3,6 +3,22 @@ import { createDropdownTypes, createPokemonList } from './dom/index.js';
 
 const addListeners = () => {
 	// Code goes here
+	const toggle = document.getElementById('header-checkbox');
+	const background = document.querySelector('.main-content');
+	const header = document.querySelector('.header__nav');
+	
+	toggle.addEventListener('change', () =>{
+		if(toggle.checked){
+			console.log("Checkbox is checked..");
+			background.classList.add('dark');
+			header.classList.add('dark');
+			
+		}else {
+			console.log("Checkbox is not checked..");
+			background.classList.remove('dark');
+			header.classList.remove('dark');
+		}
+	});
 };
 
 const main = async () => {
