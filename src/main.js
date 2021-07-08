0cd2cdbbf0899deba4d44e1fcb84189922b3e01a
Sofía Pérez Mantilla <32691511+sofiaperezm@users.noreply.github.com>
2021-07-08 00:04:07 -0500

    added theme change functionality

--- src/main.js
+++ src/main.js
@@ -3,13 +3,41 @@ import { createDropdownTypes, createPokemonList } from './dom/index.js';
 
 const addListeners = () => {
 	// Code goes here
-	const dropdownElement = document.getElementById('dropdown-types');
 
+	// Change page theme challenge
+	// Selecting the DOM element to listen to
+	const checkboxThemeElement = document.getElementById('header-checkbox');
+
+	// Selenting the event listener
+	checkboxThemeElement.addEventListener('change', (event) => {
+		const isChecked = event.target.checked;
+
+	// Selecting the DOM element to apply the CSS class
+		const mainContentElement = document.querySelector('.main-content');
+		if (isChecked) {
+			mainContentElement.classList.add('dark');
+		} else {
+			mainContentElement.classList.remove('dark');
+		}
+	})
+
+
+	// Filter the cards by Pokemon type challenge
+	// Selecting the DOM element to listen to
+	const dropdownElement = document.getElementById('dropdown-types');
+	
+	// Selecting the event listener
 	dropdownElement.addEventListener('change', (event) => {
+	
+		// Getting the option selected in the dropdown
 		const selectedType = event.target.value;
+	
+		// Getting the card items from HTML
 		const pokemonCards = document.querySelectorAll('.card-list__item');
 		pokemonCards.forEach(pokemonItem => {
+			// Getting the type of each pokemon
 			const types = pokemonItem.getAttribute('data-types');
+			// Adding or removing the hide class based on the option selected 
 			if (types.includes(selectedType) || selectedType === 'all') {
   				pokemonItem.classList.remove('hide');
 			} else {
@@ -17,6 +45,8 @@ const addListeners = () => {
 			}
 		})
 	})
+
+
 };
 
 const main = async () => {
