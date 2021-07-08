a229c657a872cbe1d1ef80a41e4ad4d8bd81c79c
MARIAN QUEVEDO <mariansofia1999@hotmail.com>
2021-07-08 17:43:15 -0500

    add filter

--- src/main.js
+++ src/main.js
@@ -6,18 +6,40 @@ const addListeners = () => {
 	const toggle = document.getElementById('header-checkbox');
 	const background = document.querySelector('.main-content');
 	const header = document.querySelector('.header__nav');
+
+	const selector = document.getElementById('dropdown-types');
+	const cardList = document.querySelectorAll('.card-list__item');
 	
+	// Theme Functionality
 	toggle.addEventListener('change', () =>{
 		if(toggle.checked){
-			console.log("Checkbox is checked..");
 			background.classList.add('dark');
 			header.classList.add('dark');
 			
 		}else {
-			console.log("Checkbox is not checked..");
 			background.classList.remove('dark');
 			header.classList.remove('dark'); 
 		}
+	});
+
+	//Filter functionality
+	selector.addEventListener('change', () =>{
+
+		const filterResult = [...cardList].filter(item => {
+			const types = item.dataset.types;
+
+			if (types.includes(selector.value) || selector.value == 'all'){
+				item.classList.remove('hide');
+				return true;
+			}else{
+				item.classList.add('hide');
+				return false;
+			}
+			
+		});
+		console.log("%cResultado Filtro", 'color: #d14136; font-family: sans-serif; font-weight: bold; font-size: 24px');
+		console.log(filterResult);
+		
 	});
 };
 
