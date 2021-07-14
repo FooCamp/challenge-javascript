f840a45ec431c391be30215fe03a2f86e8d262fa
LauraBlanco24 <laura.blanco01@correo.usa.edu.co>
2021-07-14 12:29:03 -0500

    Se agrega event listener para filtro

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
 import { getPokemonList } from './api/index.js';
 import { createDropdownTypes, createPokemonList } from './dom/index.js';
 
+
+
+const addListeners = () => {
+	let FilterType= document.querySelector('#dropdown-types').value;
+	console.log(FilterType);
+	let pokemon="";
+	let listaPokemon=document.querySelectorAll(".card")
+	listaPokemon.forEach(element => {
+		if(FilterType==="all"){
+			element.removeAttribute("style", "display:none");
+		}else{
+			let tipoPokemon=element.getAttribute("data-types");
+			if(tipoPokemon.includes(FilterType)){
+				element.removeAttribute("style", "display:none");
+			}else{
+				element.setAttribute("style", "display:none");
+			}
+		}
+	});
+	
+};
+
 const main = async () => {
 	const app = document.getElementById("app");
 	const dropdownTypes = document.getElementById("dropdown-types");
@@ -8,6 +30,9 @@ const main = async () => {
 	
 	createPokemonList(pokemonList, app);
 	createDropdownTypes(pokemonList, dropdownTypes);
+	addListeners();
 };
 
 main();
+var Filter= document.querySelector('#dropdown-types');
+Filter.addEventListener('change', addListeners);
